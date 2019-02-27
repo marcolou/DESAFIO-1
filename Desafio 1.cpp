2336ba25a1997065ea386df21e990febaf43a396
marcolou <47800242+marcolou@users.noreply.github.com>
2019-02-26 21:20:11 -0300

    Add files via upload

---
+++ Desafio 1.cpp
@@ -0,0 +1,42 @@
+// codigo em linguagem c
+
+#include <stdio.h>
+
+
+int main()
+ {
+	
+	  int A = 0;
+	  int B = 0;
+	
+	  printf("informe um valor A inteiro:\n");
+	
+	  scanf("%i", &A);
+	  
+	  printf("informe um valor B inteiro:\n");
+	  
+	  scanf("%i", &B);
+	
+
+
+     if (A > B) 
+     {
+     	printf  (" \n \n ENTAO A e maior que B");
+	 }
+	 else  
+	 {
+	 	if (A < B)
+		{
+	
+	 	printf ("\n \n ENTAO A e menor que B");
+	 	}
+	 
+	  else
+	  {
+	  	printf ("\n \n ENTAO A e igual a B");
+	  }
+   	} 
+  
+    
+			  
+}
